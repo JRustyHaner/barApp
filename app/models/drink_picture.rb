19aaa94a6b5df7b641fee0cd2f040346a1db6e1d
dankay67 <dankay67@gmail.com>
2019-03-30 03:38:56 +0000

    Added DrinkPicture validations

--- app/models/drink_picture.rb
+++ app/models/drink_picture.rb
@@ -1,2 +1,3 @@
 class DrinkPicture < ApplicationRecord
+    validates :fileName, length: { in: 3..25, wrong_length: "Filename needs to be between 3 and 25 characters."}, format: { with: /[A-Za-z0-9]/, only_letters: "Filename should only have letters."}, unique:
 end
