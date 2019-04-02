85e133e0861948e22e33f0e625bc386c62a54e94
dankay67 <dankay67@gmail.com>
2019-04-02 01:25:28 +0000

    Create ingredient model and controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  root to: 'pages#home'
+  get 'home', to: 'pages#home', as: 'home'
+  get 'about', to: 'pages#about', as: 'about'
+  get 'ingredients', to: 'pages#ingredients#index', as: 'ingredients'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
