9fdd0093a8aa9679499c4ce0d2670211e08c38cc
James Rusty Haner <jrhaner@memphis.edu>
2019-04-03 18:41:20 +0000

    Added model associations and routes

--- app/models/drink_recipe.rb
+++ app/models/drink_recipe.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: drink_recipes
+#
+#  id          :integer          not null, primary key
+#  description :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class DrinkRecipe < ApplicationRecord
+    has_many :ingredients
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  
+    get '/list/country/:country', to: 'list#country', as: 'list_by_country'
+    get '/list/ingredient/:ingredient', to: 'list#ingredient', as: 'list_by_ingredient'
 end
