b674352187511cc2ef29ffb1ca025f492629c016
James Rusty Haner <jrhaner@memphis.edu>
2019-04-03 18:47:32 +0000

    Added Routes

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: drinks
+#
+#  id            :integer          not null, primary key
+#  drinkDate     :datetime
+#  drinkLocation :string
+#  drinkName     :string
+#  specialDate   :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Drink < ApplicationRecord
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/list/top', to: 'list#top', as: 'list_top_ten'
+  get '/list/new', to: 'list#new', as: 'list_new'
 end
