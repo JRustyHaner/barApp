480532220fd7ff3c0579159b2e00ef3f474efd3a
James Rusty Haner <jrhaner@memphis.edu>
2019-04-06 20:33:17 +0000

    Added Controller Params

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,2 +1,13 @@
 class ListController < ApplicationController
+    def index
+        @drinks = Drink.all()
+    end
+    def type
+        @drinks = Drink.find_by(drinkName: params[:type])
+        @recipe = @drinks.recipe.all()
+    end
+    def occasision
+        @drinks = Drink.find_by(specialDate: params[:occasion])
+        @recipe = @drinks.recipe.all()
+    end
 end
