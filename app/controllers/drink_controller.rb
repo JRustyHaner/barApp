84d4482fdf3d7a518f7941dfee7eabb2da1eec96
dankay67 <dankay67@gmail.com>
2019-04-06 04:00:28 +0000

    Continue work on US03

--- app/controllers/drink_controller.rb
+++ app/controllers/drink_controller.rb
@@ -1,2 +1,12 @@
 class DrinkController < ApplicationController
+    def index
+        @drinks = Drink.all
+    end
+    def show
+    end
+    def new
+        @drink = Drink.new
+    end
+    def edit
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'home', to: 'pages#home', as: 'home'
   get 'about', to: 'pages#about', as: 'about'
+  get 'drink', to: 'drink#index', as: 'drink'
+  get 'drink/new', to: 'drink#new', as: 'new_drink'
+  post 'drink/create', to: 'drink#create'
+  get 'drink/:id/edit', to: 'drink#edit', as: 'edit_drink'
 end
