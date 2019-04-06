6ee791c8ef8c0d29a634d1e9c5bc0f32f9c466a5
James Rusty Haner <jrhaner@memphis.edu>
2019-04-06 20:55:31 +0000

    Edited Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/list', to: 'list#index', as: 'list_index'
   get '/list/top', to: 'list#top', as: 'list_top_ten'
   get '/list/new', to: 'list#new', as: 'list_new'
 end
