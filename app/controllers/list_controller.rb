6c3d82312196099e079d44200246059d122ec4bd
James Rusty Haner <jrhaner@memphis.edu>
2019-04-06 20:53:31 +0000

    Edited Controller

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,2 +1,13 @@
 class ListController < ApplicationController
+    def index
+        @drinks = Drink.all()
+    end
+    def top
+        @drinks = Drink.find_by(drinkName: params[:top])
+        @recipe = @drinks.recipe.all()
+    end
+    def new
+        @drinks = Drink.find(:all, :order => "id desc", :limit => 10).reverse
+        @recipe = @drinks.recipe.all()
+    end
 end
