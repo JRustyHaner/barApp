e69f93c7b0e5dba4b6ee4741e447109abd2ddf0f
dankay67 <dankay67@gmail.com>
2019-04-20 12:43:57 +0000

    Add PDF controller and PDF show page.

---
+++ app/controllers/gen_pd_fs_controller.rb
@@ -0,0 +1,20 @@
+class GenPdFsController < ApplicationController
+    def show
+        @recipe = Recipe.includes(:ingredients).find(params[:id])
+        @recipe_comment=Recipe.includes(:reviews).find(params[:id])
+        respond_to do |format|
+            format.html
+            format.pdf do
+                render pdf: @recipe.drinkName,
+                page_size: 'Letter',
+                template: "gen_pd_fs/show.html.erb",
+                layout: "pdf.html",
+                orientation: "Landscape",
+                image_quality: 5,
+                lowquality: true,
+                zoom: 1,
+                dpi: 75
+            end
+        end
+    end
+end
